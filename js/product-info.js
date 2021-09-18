e4248dd3cc202629655fdf65a02450698e8bbe68
AniB98 <anibenitez98@gmail.com>
2021-09-18 00:41:27 -0300

    Entrega 3
    -Mostrar el contenido de PRODUCT_INFO_URL
    -Mostrar el contenido de PRODUCT_INFO_COMMENTS_URL
    -Tener la posibilidad de agregar y pushear comentarios nuevos

--- js/product-info.js
+++ js/product-info.js
@@ -1,42 +1,174 @@
 var product = {};
+var productInfo = [];
+let comment = [];
+let newCommentInfo = {};
 
-function showImagesGallery(array){
+//Esta función recibe y recorre un array y agrega en el HTML (en el ID productImagesGallery) imágenes 
+function showImagesGallery(array) {
+	let htmlContentToAppend = "";
 
-    let htmlContentToAppend = "";
+	for (let i = 0; i < array.length; i++) {
+		let imageSrc = array[i];
 
-    for(let i = 0; i < array.length; i++){
-        let imageSrc = array[i];
-
-        htmlContentToAppend += `
+		htmlContentToAppend +=
+			`
         <div class="col-lg-3 col-md-4 col-6">
             <div class="d-block mb-4 h-100">
-                <img class="img-fluid img-thumbnail" src="` + imageSrc + `" alt="">
+                <img class="img-fluid img-thumbnail" src="` +
+			imageSrc +
+			`" alt="">
             </div>
         </div>
-        `
+        `;
 
-        document.getElementById("productImagesGallery").innerHTML = htmlContentToAppend;
-    }
+		document.getElementById("productImagesGallery").innerHTML = htmlContentToAppend;
+	}
 }
 
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCT_INFO_URL).then(function(resultObj){
-        if (resultObj.status === "ok")
-        {
-            product = resultObj.data;
+//Esta función recibe y recorre un array y agrega en el HTML (en el ID relatedProducts) imagen, nombre y descripción
+function productRel(array) {
+	let htmlContentToAppend = "";
 
-            let productNameHTML  = document.getElementById("productName");
-            let productDescriptionHTML = document.getElementById("productDescription");
-            let productCountHTML = document.getElementById("productCount");
-            let productCriteriaHTML = document.getElementById("productCriteria");
-        
-            productNameHTML.innerHTML = product.name;
-            productDescriptionHTML.innerHTML = product.description;
-            productCountHTML.innerHTML = product.productCount;
-            productCriteriaHTML.innerHTML = product.productCriteria;
+	for (let dato of array) {
+		//product está definido ...
+		let relatedProductsIndex = product[dato];
 
-            //Muestro las imagenes en forma de galería
-            showImagesGallery(product.images);
-        }
-    });
+		// `lo que esté acá adentro es un literal template ` y eso puede contener placeholders (${expression})
+		// el contendio dentro del placeholder pasa como función 
+		htmlContentToAppend += `
+        <span class="card mr-3" style="width: 18rem; display: inline-block">
+  					<img src="${relatedProductsIndex.imgSrc}" class="card-img-top">
+  				<div class="card-body">
+    				<h5 class="card-title">${relatedProductsIndex.name}</h5>
+    				<p class="card-text">${relatedProductsIndex.description}</p>
+  				</div>
+  				<div class="card-body">
+    				<a href="#" class="card-link">Ver Producto</a>
+  				</div>
+			</span>
+    `;
+	}
+	document.getElementById("relatedProducts").innerHTML = htmlContentToAppend;
+}
+
+// Este Listener funciona una vez que carga el HTML
+document.addEventListener("DOMContentLoaded", function (e) {
+	//Obtengo la información JSON del URL definido en init.js y entro al if si no hubieron errores
+	getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
+		if (resultObj.status === "ok") {
+			productInfo = resultObj.data;
+
+			//Aquí asocio una variable con un lugar del HTML por su id
+			let productNameHTML = document.getElementById("productName");
+			let productPriceHTML = document.getElementById("productPrice");
+			let productCategoryHTML = document.getElementById("productCategory");
+			let productDescriptionHTML =
+				document.getElementById("productDescription");
+			let productCountHTML = document.getElementById("soldCount");
+
+			//Aquí asocio una parte específica de productInfo con el innerHTML de 
+			//las variables definidas anteriormente
+			productNameHTML.innerHTML = productInfo.name;
+			productPriceHTML.innerHTML = productInfo.cost + productInfo.currency;
+			productCategoryHTML.innerHTML = productInfo.category;
+			productDescriptionHTML.innerHTML = productInfo.description;
+			productCountHTML.innerHTML = productInfo.soldCount;
+
+			//Muestro las imagenes en forma de galería usando la función definida en la línea 49
+			//(showImagesGallery). Toma todas la imágenes que se encuentran en productInfo)
+			showImagesGallery(productInfo.images);
+		}
+
+		//Obtengo la información JSON del URL definido en init.js y entro al if si no hubieron errores
+		getJSONData(PRODUCTS_URL).then(function (resultObj) {
+			if (resultObj.status === "ok") {
+				product = resultObj.data;
+			}
+			//Aquí corro la función productRel (definida anteriormente, linea 27)
+			// y le paso los parámetros obtenidos con el JSON
+			//productInfo (definido anteriormente, linea 86) y relatedProducts
+			productRel(productInfo.relatedProducts);
+		});
+	});
 });
+//Esta función recibe y recorre un array y agrega en el HTML (en el ID comments)
+//user, score, descripción, día y hora
+function showComments(comments) {
+	
+	let htmlContentToAppend = "";
+	
+	for (let item of comments) {
+		
+		htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+        <div class="row">
+            <div class="col">
+                <div class="d-flex w-100 justify-content-between">
+                    <div class="mb-1">
+                        <h6 class="font-weight-bold margin-score">${item.user} ${showScore(item.score)} </h6>
+                        <p>${item.description}</p>
+                    </div>
+                    <small class="text-muted">${item.dateTime}</small>
+                </div>
+            </div>
+        </div>
+    	</div>
+        `;
+		document.getElementById("comments").innerHTML = htmlContentToAppend;
+	}
+}
+//Obtengo la información JSON del URL definido en init.js y entro al if si no hubieron errores
+getJSONData(PRODUCT_INFO_COMMENTS_URL).then(function (resultObj) {
+	if (resultObj.status === "ok") {
+		comment = resultObj.data;
+	}
+	showComments(comment);
+});
+
+
+//Esta función toma el score del JSON de cada comentario y asocia ese numero a un display
+//y así se ve la puntuación asociada al comentario en forma de estrellas
+function showScore(score) {
+	let stars = "";
+
+	for (let i = 1; i <=5; i++) {
+		if (i<=score){
+		stars += `<i class="fas fa-star"></i>`;	
+		}
+		else {
+			stars += `<i class="far fa-star"></i>`;
+		}
+	}
+	return stars
+}
+
+//DESAFÍATE
+
+//La variable today tendrá la fecha y hora actual con el formato deseado
+var today = new Date();
+var date = today.getFullYear()+'-'+ (today.getMonth()+1)+'-'+ today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+document.getElementById("currentDayTime").innerHTML = date + ' ' + time
+
+//Esta función agrega un nuevo comentario al resultado del getJSONData(PRODUCT_INFO_COMMENTS_URL)
+//con los parámetros que suele tener (usuarios, descripción, puntuación, fecha y hora)
+//y los devuelve como un comenario más, solo que al actualizar se elimina
+//es decir que no se hace el push en el JSON mismo
+function showNewComment(){
+
+	let newCommentScore = document.getElementById("newScore").textContent ;
+	let newCommentDescription = document.getElementById("description").value ;
+	let newCommentUser = localStorage.getItem("user") ;
+	let newCommentTime = document.getElementById("currentDayTime").textContent;
+	
+	let newCommentInfo = {
+		"score": newCommentScore,
+		"description": newCommentDescription,
+		"user": newCommentUser,
+		"dateTime": newCommentTime	
+	};
+
+	comment.push(newCommentInfo);
+	showComments(comment);
+	document.getElementById("description").value = "";	
+};
--- js/products.js
+++ js/products.js
@@ -8,213 +8,253 @@ var maxCount = undefined;
 
 //Función que filtra según un criterio
 //(Por precio- Ascendente y descendente, por cantidad de vendidos)
-function sortProducts(criteria, array){
-    let result = [];
-    if (criteria === ORDER_ASC_BY_PRICE)
-    {
-        result = array.sort(function(a, b) {
-            if ( a.cost < b.cost ){ return -1; }
-            if ( a.cost > b.cost ){ return 1; }
-            return 0;
-        });
-    }else if (criteria === ORDER_DESC_BY_PRICE){
-        result = array.sort(function(a, b) {
-            if ( a.cost > b.cost ){ return -1; }
-            if ( a.cost < b.cost ){ return 1; }
-            return 0;
-        });
-    }else if (criteria === ORDER_BY_SOLD_COUNT){
-        result = array.sort(function(a, b) {
-            let aCount = parseInt(a.soldCount);
-            let bCount = parseInt(b.soldCount);
+function sortProducts(criteria, array) {
+	let result = [];
+	if (criteria === ORDER_ASC_BY_PRICE) {
+		result = array.sort(function (a, b) {
+			if (a.cost < b.cost) {
+				return -1;
+			}
+			if (a.cost > b.cost) {
+				return 1;
+			}
+			return 0;
+		});
+	} else if (criteria === ORDER_DESC_BY_PRICE) {
+		result = array.sort(function (a, b) {
+			if (a.cost > b.cost) {
+				return -1;
+			}
+			if (a.cost < b.cost) {
+				return 1;
+			}
+			return 0;
+		});
+	} else if (criteria === ORDER_BY_SOLD_COUNT) {
+		result = array.sort(function (a, b) {
+			let aCount = parseInt(a.soldCount);
+			let bCount = parseInt(b.soldCount);
 
-            if ( aCount > bCount ){ return -1; }
-            if ( aCount < bCount ){ return 1; }
-            return 0;
-        });
-    }
-    return result;
+			if (aCount > bCount) {
+				return -1;
+			}
+			if (aCount < bCount) {
+				return 1;
+			}
+			return 0;
+		});
+	}
+	return result;
 }
 
 //Función que muestra la lista de productos, recorriendo la lista del JSON (procesado más abajo),
 //luego hace append del resultado en el HTML (products.html) en el div correspondiente (id="prod-list-container")
-function showProductList(){
+function showProductList() {
+	let htmlContentToAppend = "";
+	for (let i = 0; i < CurrentProductArray.length; i++) {
+		let product = CurrentProductArray[i];
 
-    let htmlContentToAppend = "";
-    for(let i = 0; i < CurrentProductArray.length; i++){
-        let product = CurrentProductArray[i];
-
-        //Undefined es el valor inicial, en ese caso siempre entra en el if
-        //pero si se agregó valor en la parte de min o max count, toma eso como valor y lo compara al cost
-        //Si el cost entra en el rango entonces entre en el if 
-        //Más abajo se define el minCount y maxCount
-        if (((minCount == undefined) || (minCount != undefined && parseInt(product.cost) >= minCount)) &&
-            ((maxCount == undefined) || (maxCount != undefined && parseInt(product.cost) <= maxCount))){
-            
-            //Crea el contenido para el append del HTML
-            //Selecciona Imagen, Descripción, nombre del producto, precio y moneda, cantidad de vendidos
-            htmlContentToAppend += `
+		//Undefined es el valor inicial, en ese caso siempre entra en el if
+		//pero si se agregó valor en la parte de min o max count, toma eso como valor y lo compara al cost
+		//Si el cost entra en el rango entonces entre en el if
+		//Más abajo se define el minCount y maxCount
+		if (
+			(minCount == undefined ||
+				(minCount != undefined && parseInt(product.cost) >= minCount)) &&
+			(maxCount == undefined ||
+				(maxCount != undefined && parseInt(product.cost) <= maxCount))
+		) {
+			//Crea el contenido para el append del HTML
+			//Selecciona Imagen, Descripción, nombre del producto, precio y moneda, cantidad de vendidos
+			htmlContentToAppend +=
+				`
             <a href="product-info.html" class="list-group-item list-group-item-action">
                 <div class="row">
                     <div class="col-3">
-                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                        <img src="` +
+				product.imgSrc +
+				`" alt="` +
+				product.description +
+				`" class="img-thumbnail">
                     </div>
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">`+ product.name +`</h4>
-                            <small class="text-muted">` + product.cost + ` ` + product.currency + `</small>
+                            <h4 class="mb-1">` +
+				product.name +
+				`</h4>
+                            <small class="text-muted">` +
+				product.cost +
+				` ` +
+				product.currency +
+				`</small>
                         </div>
-                        <p class="mb-1">` + product.description + `</p> </br>
-                        <p class="mb-1">` + product.soldCount + ` artículos vendidos</p>
+                        <p class="mb-1">` +
+				product.description +
+				`</p> </br>
+                        <p class="mb-1">` +
+				product.soldCount +
+				` artículos vendidos</p>
                     </div>
                 </div>
             </a>
-            `
-        }
+            `;
+		}
 
-        document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
-    }
+		document.getElementById("prod-list-container").innerHTML =
+			htmlContentToAppend;
+	}
 }
 
 // Hace que si hay un criterio de filtro seleccionado se muestre la lista con ese filtro aplicado
-//Asocia a la función ya definida más arriba (sortProducts) a la nueva función sortAndShowProducts 
-function sortAndShowProducts(sortCriteria, productsArray){
-    currentSortCriteria = sortCriteria;
+//Asocia a la función ya definida más arriba (sortProducts) a la nueva función sortAndShowProducts
+function sortAndShowProducts(sortCriteria, productsArray) {
+	currentSortCriteria = sortCriteria;
 
-    if(productsArray != undefined){
-        CurrentProductArray = productsArray;
-    }
+	if (productsArray != undefined) {
+		CurrentProductArray = productsArray;
+	}
 
-    CurrentProductArray = sortProducts(currentSortCriteria, CurrentProductArray);
+	CurrentProductArray = sortProducts(currentSortCriteria, CurrentProductArray);
 
-    //Llama a la función showProductList
-    showProductList();
+	//Llama a la función showProductList
+	showProductList();
 }
 
 //Esto se ejecuta una vez que el documento se encuentra cargado y funcionando.
 //PRODUCTS_URL está definido en init.js
 //Se asocian las funciones con los eventos clicks para que se ejecuten los filtros
-document.addEventListener("DOMContentLoaded", function(e){
-    getJSONData(PRODUCTS_URL).then(function(resultObj){
-        if (resultObj.status === "ok"){
-            sortAndShowProducts(ORDER_ASC_BY_PRICE, resultObj.data);
-        }
-    });
+document.addEventListener("DOMContentLoaded", function (e) {
+	getJSONData(PRODUCTS_URL).then(function (resultObj) {
+		if (resultObj.status === "ok") {
+			sortAndShowProducts(ORDER_ASC_BY_PRICE, resultObj.data);
+		}
+	});
 
-    document.getElementById("sortAsc").addEventListener("click", function(){
-        sortAndShowProducts(ORDER_ASC_BY_PRICE);
-    });
+	document.getElementById("sortAsc").addEventListener("click", function () {
+		sortAndShowProducts(ORDER_ASC_BY_PRICE);
+	});
 
-    document.getElementById("sortDesc").addEventListener("click", function(){
-        sortAndShowProducts(ORDER_DESC_BY_PRICE);
-    });
+	document.getElementById("sortDesc").addEventListener("click", function () {
+		sortAndShowProducts(ORDER_DESC_BY_PRICE);
+	});
 
-    document.getElementById("sortByCount").addEventListener("click", function(){
-        sortAndShowProducts(ORDER_BY_SOLD_COUNT);
-    });
+	document.getElementById("sortByCount").addEventListener("click", function () {
+		sortAndShowProducts(ORDER_BY_SOLD_COUNT);
+	});
 
-    document.getElementById("clearRangeFilter").addEventListener("click", function(){
-        document.getElementById("rangeFilterCountMin").value = "";
-        document.getElementById("rangeFilterCountMax").value = "";
+	document
+		.getElementById("clearRangeFilter")
+		.addEventListener("click", function () {
+			document.getElementById("rangeFilterCountMin").value = "";
+			document.getElementById("rangeFilterCountMax").value = "";
 
-        minCount = undefined;
-        maxCount = undefined;
+			minCount = undefined;
+			maxCount = undefined;
 
-        //Llama a la función showProductList
-        showProductList();
-    });
+			//Llama a la función showProductList
+			showProductList();
+		});
 
-    //Obtengo el mínimo y máximo de los intervalos ingresados en el HTML
-    //y los asocio a minCount y maxCount, que se utilizan más arriba
-    document.getElementById("rangeFilterCount").addEventListener("click", function(){
-        
-        minCount = document.getElementById("rangeFilterCountMin").value;
-        maxCount = document.getElementById("rangeFilterCountMax").value;
+	//Obtengo el mínimo y máximo de los intervalos ingresados en el HTML
+	//y los asocio a minCount y maxCount, que se utilizan más arriba
+	document
+		.getElementById("rangeFilterCount")
+		.addEventListener("click", function () {
+			minCount = document.getElementById("rangeFilterCountMin").value;
+			maxCount = document.getElementById("rangeFilterCountMax").value;
 
-        if ((minCount != undefined) && (minCount != "") && (parseInt(minCount)) >= 0){
-            minCount = parseInt(minCount);
-        }
-        else{
-            minCount = undefined;
-        }
+			if (minCount != undefined && minCount != "" && parseInt(minCount) >= 0) {
+				minCount = parseInt(minCount);
+			} else {
+				minCount = undefined;
+			}
 
-        if ((maxCount != undefined) && (maxCount != "") && (parseInt(maxCount)) >= 0){
-            maxCount = parseInt(maxCount);
-        }
-        else{
-            maxCount = undefined;
-        }
-        
-        //Llama a la función showProductList
-        showProductList();
-    });
+			if (maxCount != undefined && maxCount != "" && parseInt(maxCount) >= 0) {
+				maxCount = parseInt(maxCount);
+			} else {
+				maxCount = undefined;
+			}
 
-    //Desafíte - Search Bar
-    
-    //esta constante se crea mediante el contenido del id prod-list-container, que tiene toda la
-    //lista de productos según el filtro de ese momento
-    const productList = document.getElementById("prod-list-container");
-    //esta constante se crea obteniendo el contenido del input ingresado por el ususario en al search bar
-    const searchBar = document.getElementById("search-bar");
-    let products = [];
-    
-    searchBar.addEventListener('keyup', (e) => {
-        //esta constante retorna el valor generado por el evento de soltar la tecla
-        //y lo pasa a minúsculas
-        const searchString = e.target.value.toLowerCase();
-    
-        //esta constante retorna los elmentos de "product" que cumplen  
-        //con las condiciones espcificadas en la cte searchString
-        //y retorna el/los resultado de producto/s que cumpla/n con esa condición
-        const filteredProducts = products.filter((product) => {
-            return (
-                product.name.toLowerCase().includes(searchString)
-            );
-        });
-        displayProducts(filteredProducts);
-    });
-    
-    //esta constante actúa como una función asincrónica
-    //que carga los productos desde el URL asociado a PRODUCTS_URL
-    //intentamostrar productos y en caso de no poder, un error
-    const loadProducts = async () => {
-        try {
-            const res = await fetch(PRODUCTS_URL);
-            products = await res.json();
-            displayProducts(products);
-        } catch (err) {
-            console.error(err);
-        }
-    };
-    
-    //aquí se inserta en el HTML el contenido obtenido según el filtro aplicado con la Search Bar
-    const displayProducts = (products) => {
-        const htmlString = products
-            .map((product) => {
-                return `<a href="product-info.html" class="list-group-item list-group-item-action">
+			//Llama a la función showProductList
+			showProductList();
+		});
+
+	//Desafíte - Search Bar
+
+	//esta constante se crea mediante el contenido del id prod-list-container, que tiene toda la
+	//lista de productos según el filtro de ese momento
+	const productList = document.getElementById("prod-list-container");
+	//esta constante se crea obteniendo el contenido del input ingresado por el ususario en al search bar
+	const searchBar = document.getElementById("search-bar");
+	let products = [];
+
+	searchBar.addEventListener("keyup", (e) => {
+		//esta constante retorna el valor generado por el evento de soltar la tecla
+		//y lo pasa a minúsculas
+		const searchString = e.target.value.toLowerCase();
+
+		//esta constante retorna los elmentos de "product" que cumplen
+		//con las condiciones espcificadas en la cte searchString
+		//y retorna el/los resultado de producto/s que cumpla/n con esa condición
+		const filteredProducts = products.filter((product) => {
+			return product.name.toLowerCase().includes(searchString);
+		});
+		displayProducts(filteredProducts);
+	});
+
+	//esta constante actúa como una función asincrónica
+	//que carga los productos desde el URL asociado a PRODUCTS_URL
+	//intentamostrar productos y en caso de no poder, un error
+	const loadProducts = async () => {
+		try {
+			const res = await fetch(PRODUCTS_URL);
+			products = await res.json();
+			displayProducts(products);
+		} catch (err) {
+			console.error(err);
+		}
+	};
+
+	//aquí se inserta en el HTML el contenido obtenido según el filtro aplicado con la Search Bar
+	const displayProducts = (products) => {
+		const htmlString = products
+			.map((product) => {
+				return (
+					`<a href="product-info.html" class="list-group-item list-group-item-action">
                 <div class="row">
                     <div class="col-3">
-                        <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                        <img src="` +
+					product.imgSrc +
+					`" alt="` +
+					product.description +
+					`" class="img-thumbnail">
                     </div>
                     <div class="col">
                         <div class="d-flex w-100 justify-content-between">
-                            <h4 class="mb-1">`+ product.name +`</h4>
-                            <small class="text-muted">` + product.cost + ` ` + product.currency + `</small>
+                            <h4 class="mb-1">` +
+					product.name +
+					`</h4>
+                            <small class="text-muted">` +
+					product.cost +
+					` ` +
+					product.currency +
+					`</small>
                         </div>
-                        <p class="mb-1">` + product.description + `</p> </br>
-                        <p class="mb-1">` + product.soldCount + ` artículos vendidos</p>
+                        <p class="mb-1">` +
+					product.description +
+					`</p> </br>
+                        <p class="mb-1">` +
+					product.soldCount +
+					` artículos vendidos</p>
                     </div>
                 </div>
             </a>
-            `;
-            })
-            .join('');
-        productList.innerHTML = htmlString;
-    };
-    
-    loadProducts();
-    
-});
+            `
+				);
+			})
+			.join("");
+		productList.innerHTML = htmlString;
+	};
 
-    
-        
+	loadProducts();
+});
